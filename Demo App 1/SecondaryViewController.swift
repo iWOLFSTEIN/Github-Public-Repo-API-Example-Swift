ca053bc0e1fbb64da3ca28b4b8535e7788e857cd
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-05-03 13:10:53 +0500

    everything working

---
+++ Demo App 1/SecondaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondaryViewController.swift
+//  Demo App 1
+//
+//  Created by BrainX Technologies on 03/05/2023.
+//
+
+import UIKit
+
+class SecondaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
